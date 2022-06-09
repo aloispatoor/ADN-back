96865864edb2febfe105032af449ceb15c6b08c6
Alois Patoor <palois89@posteo.net>
2022-06-09 10:27:12 +0200

    just to be sure the git is updated

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,7 +6,6 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use App\Form\ArticleType;
 use App\Form\CommentType;
-use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
