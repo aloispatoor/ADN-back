b7cf4ca6542d530b670953b35ad4be4e792d7de2
Alois Patoor <palois89@posteo.net>
2023-02-06 11:34:59 +0100

    MailChimpService Basic Auth

--- src/Service/MailchimpService.php
+++ src/Service/MailchimpService.php
@@ -1,12 +1,20 @@
 <?php
 require_once '../../vendor/autoload.php';
 
-$mailchimp = new MailchimpMarketing\ApiClient();
 
-$mailchimp->setConfig([
-  'apiKey' => $this->getParameter('app.apikey'),
-  'server' => $this->getParameter('app.serverprefix')
-]);
+class MailchimpService {
 
-$response = $mailchimp->ping->get();
-print_r($response);
+    public function mailchimpAuth(): void
+    {
+        $mailchimp = new MailchimpMarketing\ApiClient();
+
+        $mailchimp->setConfig([
+          'apiKey' => $this->getParameter('app.apikey'),
+          'server' => $this->getParameter('app.serverprefix')
+        ]);
+
+        $response = $mailchimp->ping->get();
+        print_r($response);
+    }
+}
+
